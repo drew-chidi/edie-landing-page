f76de51ac9ef04a6104045687e2a73fba5d0ccb3
drew-chidi <chidi.andrew@gmail.com>
2021-11-19 22:18:25 +0100

    adjusted the mobile nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <ul
           className={`${
             open ? "fixed z-10 top-0 left-0 bg-gray-300" : "hidden"
-          } lg:bg-transparent h-full w-3/5 lg:static lg:flex justify-end lg:w-full font-medium`}
+          } lg:bg-transparent h-full w-3/5 pt-12 lg:pt-0 lg:static lg:flex justify-end lg:w-full font-medium`}
         >
           <li className='mx-8'>
             <a
@@ -66,7 +66,7 @@ const Navbar = () => {
         <div className='lg:hidden flex w-full justify-end items-center'>
           <AiOutlineMenu
             onClick={navToggleHandler}
-            className='w-4 h-3.5 text-black font-medium'
+            className='w-4 h-3.5 text-black font-bold'
           />
         </div>
         <nav></nav>
